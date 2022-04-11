8b461b38499bc3126a654a0f585157d2af424f0e
Guy Bramsworth <mpdev1987@gmail.com>
2022-04-10 20:24:17 -0700

    init

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,9 +2,20 @@ import Firebase from 'firebase/compat/app'
 import 'firebase/compat/firestore'
 import 'firebase/compat/auth'
 
-const config = {}
+// import { seedDatabase } from '../seed'
+
+const config = {
+    apiKey: "null",
+    authDomain: "instaclone-5233b.firebaseapp.com",
+    projectId: "instaclone-5233b",
+    storageBucket: "instaclone-5233b.appspot.com",
+    messagingSenderId: "847168005844",
+    appId: "1:847168005844:web:c832c08af6b9d6fc66fd41"
+}
 
 const firebase = Firebase.initializeApp(config)
 const { FieldValue } = Firebase.firestore
+
+// seedDatabase(firebase)
 
 export { firebase, FieldValue }
